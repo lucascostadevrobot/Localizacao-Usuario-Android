b14827426f27b53b2a11c40b3e029e531f74d236
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2022-09-20 16:41:37 -0300

    commit v20 recuperando location user
    -recuperando localizacao do usuario

--- app/src/main/java/lucascosta/dev/localizacaousuario/activitys/MapsActivity.java
+++ app/src/main/java/lucascosta/dev/localizacaousuario/activitys/MapsActivity.java
@@ -13,6 +13,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -26,6 +28,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import lucascosta.dev.localizacaousuario.R;
 import lucascosta.dev.localizacaousuario.databinding.ActivityMapsBinding;
@@ -41,10 +47,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     //Adicionando as permissões na classe principal que precisamos trabalhar
     //Para isso criaremos um array de Strings com o nome de permissoes.
-    private String[] permissoes =  new String[]{
+    private String[] permissoes = new String[]{
             Manifest.permission.ACCESS_FINE_LOCATION
     };
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,21 +63,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Chamando nosso objetivo permissoes para validar
         Permissoes.validarPermissoes(permissoes, this, 1);
 
-        //Objeto responsável por gerenciar a localização do usuário
-        locationManager =  (LocationManager) this.getSystemService(Context.LOCATION_SERVICE); //Recuperamos locationmanager
-        //Objeto instanciado do nosso atributo locationListener
-        locationListener = new LocationListener() {
-            @Override
-            //
-            public void onLocationChanged( @NonNull  Location location) {
-                Log.d("Localização","onLocationChanged: " + location.toString());
-            }
-        };
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     /**
@@ -82,21 +79,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //Objeto responsável por gerenciar a localização do usuário
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE); //Recuperamos locationmanager
+        //Objeto instanciado do nosso atributo locationListener
+        locationListener = new LocationListener() {
+            @Override
+            //
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("Localização", "onLocationChanged: " + location.toString());
+                // Add a marker in Sydney and move the camera
+                //Criando variavel latitude e longitude no escopo do metodo
+                mMap.clear(); //Limpar os marcados antes de criar um novo
+                Double latitude = location.getLatitude();
+                Double longitude = location.getLongitude();
+                LatLng localusuario = new LatLng(latitude, longitude);
+                mMap.addMarker(new MarkerOptions().position(localusuario).title("Meu Local"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localusuario, 25)); //Dando zoom na localizaçao do usuario dentro do mapa
 
 
-        // Add a marker in Sydney and move the camera
-        LatLng itaperuna = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(itaperuna).title("Unimed Norte Fluminense"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(itaperuna));
+                /*
+                * Geocoding -> processo de transformar um endereço ou descrição de um local em latitude/longitude
+                * Reverse Geocoding -> processo de transformar latitude/longitude em um endereço
+                * */
 
-        //Metodo  para dar zoom no mapa através do moveCamera e passando CameraUpdateFactory
-        mMap.moveCamera(
-                CameraUpdateFactory.newLatLngZoom(itaperuna, 15)
-        );
+                //1º Começaremos a realizar o processo de Geocoding
+                //Locale.getDefault é necessário para que o Geocoder saiba a localizacao que vai tratar os endereços
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listaEndereco = geocoder.getFromLocation(latitude, longitude, 1); //Podemos definir quantos resultados queremos (double lat, double long, int maxResults)
+                    if(listaEndereco != null && listaEndereco.size() > 0){
+                        Address endereco = listaEndereco.get(0);
+                        endereco.toString(); //Convertendo objeto endereços em strings
+                        Log.d("local", "OnlocationChanged: ");
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        /* Segunda vez que executamos o metodo de gerenciamento de permissões.
+         * 1)Provedor da localização
+         * 2)Tempo mínimo entre atualizações de localizações (milesegundos)
+         * 3)Distancia mínima entre atualizações de localizações
+         * 4)Location listener (para recebermos as atualizações)
+         */
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0,
+                    0,
+                    locationListener
+            ); //Atualizando a localizacao do user
+        }
 
         //Chamando o arquivo json com a estilização do mapa através desse metodo
         //Através dessa funcionalidade conseguimos chamar um mapa estilizado no arquivo JSON
@@ -161,12 +217,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             builder.setTitle("Permissões Negagadas");
             builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
             builder.setCancelable(false);
-            builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    finish();
-                }
-            });
+            builder.setPositiveButton("Confirmar", (dialogInterface, i) -> finish());
 
             AlertDialog dialog = builder.create();
             dialog.show();
